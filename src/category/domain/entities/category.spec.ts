50df25e74d3d7ab02dae84bb5f65a86e5beb7ca1
andrewscarlos <andrews.knct@gmail.com>
2022-08-11 23:09:28 -0300

    test: create unit test for ctegory entity

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,9 +1,113 @@
-import {Category} from "./category";
+import { Category } from "./category";
+import { omit } from "lodash";
+describe("Category Unit Tests", () => {
+  it("constructor of category", () => {
+    let category = new Category({ name: "Movie" });
+    let props = omit(category.props, "created_at");
+    expect(props).toStrictEqual({
+      name: "Movie",
+      description: null,
+      is_active: true,
+    });
+    expect(category.props.created_at).toBeInstanceOf(Date);
 
-describe("Category Test", ()=>{
-    it("should be created", ()=>{
-        const category = new Category("andrews");
-        expect(category).toBeTruthy();
-    }
-    );
-})
+    let createdAt = new Date();
+    category = new Category({
+      name: "Movie",
+      description: "Some Description",
+      is_active: false,
+    });
+
+    expect(category.props).toStrictEqual({
+      name: "Movie",
+      description: "Some Description",
+      is_active: false,
+      created_at: createdAt,
+    });
+
+    category = new Category({
+      name: "Movie",
+      description: "Other Description",
+    });
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      description: "Other Description",
+    });
+
+    category = new Category({
+      name: "Movie",
+      is_active: true,
+    });
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      is_active: true,
+    });
+
+    category = new Category({
+      name: "Movie",
+      created_at: createdAt,
+    });
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      created_at: createdAt,
+    });
+  });
+  it("getter of name field", () => {
+    const category = new Category({ name: "Movie" });
+    expect(category.name).toBe("Movie");
+  });
+
+  it("getter and setter of description field", () => {
+    let category = new Category({
+      name: "Movie",
+      description: "Some description",
+    });
+    expect(category.description).toBe("Some description");
+    category = new Category({
+      name: "Movie",
+    });
+    expect(category.description).toBe(null);
+
+    category = new Category({
+      name: "Movie",
+    });
+    category["description"] = "Other description";
+    expect(category.description).toBe("Other description");
+
+    category["description"] = undefined;
+    expect(category.description).toBeNull();
+  });
+
+  it("getter and setter of is_active field", () => {
+    let category = new Category({
+      name: "Movie",
+      is_active: true,
+    });
+    expect(category.is_active).toBeTruthy();
+
+    category = new Category({
+      name: "Movie",
+    });
+    expect(category.is_active).toBeTruthy();
+
+    category = new Category({
+      name: "Movie",
+      is_active: false,
+    });
+    expect(category.is_active).toBeFalsy();
+  });
+
+  it("getter of created_at prop", () => {
+    let category = new Category({
+      name: "Movie",
+    });
+    expect(category.created_at).toBeInstanceOf(Date);
+
+    let created_at = new Date();
+    category = new Category({
+      name: "Movie",
+      created_at,
+    });
+    expect(category.created_at).toBe(created_at);
+  });
+});
--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,4 +1,33 @@
-export class Category{
-constructor (public name: string){}
+export type CategoryProperties = {
+  name: string;
+  description?: string;
+  is_active?: boolean;
+  created_at?: Date;
+};
+export class Category {
+  constructor(public readonly props: CategoryProperties) {
+    this.description = this.props.description;
+    this.props.is_active = this.props.is_active ?? true;
+    this.props.created_at = this.props.created_at ?? new Date();
+  }
+  get name(): string {
+    return this.props.name;
+  }
+  get description(): string | undefined {
+    return this.props.description;
+  }
+  get is_active(): boolean | undefined {
+    return this.props.is_active;
+  }
+  get created_at(): Date | undefined {
+    return this.props.created_at;
+  }
 
+  private set description(value: string) {
+    this.props.description = value ?? null;
+  }
+
+  private set is_active(value: boolean) {
+    this.props.is_active = value ?? true;
+  }
 }
